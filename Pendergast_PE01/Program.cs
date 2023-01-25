48b97dd646e0d081450c3c096c931bbb4d8e63cd
Chloe Pendergast <cmp2650@rit.edu>
2023-01-24 22:11:49 -0500

    Hello World PE01

---
+++ Pendergast_PE01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pendergast_PE01
+{
+    // Class: Program
+    // Author: Chloe Pendergast
+    // Purpose: Console Read/Write and Exception-Handling exercise
+    // Restrictions: None
+
+    static internal class Program
+    {
+        // Method: Main
+        // Purpose: Outputs hello world
+        // Restrictions: None
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
