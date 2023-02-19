854c0ed24bdcaa936b821b9997aaf3b2c496c66f
Chloe Pendergast <cmp2650@rit.edu>
2023-02-18 23:34:44 -0500

    raise with struct

---
+++ Pendergast_UnitTest1-6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pendergast_UnitTest1_5
+{
+    internal class Program
+    {
+        struct employee
+        {
+            public string sName;
+            public double dSalary;
+            public string name;
+        }
+        static void Main(string[] args)
+        {
+           employee employee = new employee();
+            employee.sName = "Chloe";
+            employee.dSalary = 30000;
+            Console.WriteLine("Please enter your name.");
+            employee.name = Console.ReadLine();
+            GiveRaise(employee);
+        }
+        static bool GiveRaise(employee employee)
+        {
+            if (string.Equals(employee.name, employee.sName, StringComparison.CurrentCultureIgnoreCase))
+            {
+                employee.dSalary += 19999.99;
+                Console.WriteLine("Well done " + employee.name + "!");
+                Console.WriteLine("Your salary has been increased to: " + employee.dSalary);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("We regret to inform you, " + employee.name + ", you are staying poor.");
+                return false;
+            }
+        }
+    }
+}
