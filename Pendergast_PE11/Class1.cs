266e41c4d7643f33d3fa5115d4466998979380a0
Chloe Pendergast <cmp2650@rit.edu>
2023-03-07 21:40:11 -0500

    PE11 Part 1

---
+++ Pendergast_PE11/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pendergast_PE11
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+    }
+    public abstract class Car : Vehicle
+    {
+
+    }
+    public abstract class Train : Vehicle
+    {
+
+    }
+    public class Compact : Car, IPassengerCarrier
+    {
+
+    }
+    public class SUV : Car, IPassengerCarrier
+    {
+
+    }
+    public class Pickup : Car, IPassengerCarrier
+    {
+
+    }
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+
+    }
+    public class FreightTrain : Train, IHeavyLoadCarrier
+    {
+
+    }
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+
+    }
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
+    public interface IHeavyLoadCarrier
+    {
+
+    }
+}
