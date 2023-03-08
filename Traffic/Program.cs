f5e1d7f970a6c94d3ed0cdda224de8022b71c8ed
Chloe Pendergast <cmp2650@rit.edu>
2023-03-07 21:50:34 -0500

    PE11 part 2

---
+++ Traffic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using Pendergast_PE11;
+
+namespace Traffic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPassengerCarrier iPassengerCarrier = null;
+            Compact compact = new Compact();
+            FreightTrain freightTrain = new FreightTrain();
+            AddPassenger(compact);
+            AddPassenger(freightTrain);
+
+            void AddPassenger(object vehicleObject)
+            {
+                iPassengerCarrier = (IPassengerCarrier) vehicleObject;
+                iPassengerCarrier.LoadPassenger();
+                Console.WriteLine(vehicleObject.ToString());
+            }
+        }
+    }
+}
