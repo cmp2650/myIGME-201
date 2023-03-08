e52a916feafaf3c8123cf56666205a23b80ad3fc
Chloe Pendergast <cmp2650@rit.edu>
2023-03-07 22:28:36 -0500

    PE12

---
+++ Pendergast_PE12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pendergast_PE12
+{
+    public class MyClass
+    {
+        string myString;
+        string MyString
+        {
+            set { myString = value; }
+        }
+        public virtual string GetString()
+        {
+            return myString;
+        }
+    }
+
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            // calls base operator of GetString and adds text to teh returned string
+            return base.GetString() + " output from the derived class.";
+        }
+    }
+}
