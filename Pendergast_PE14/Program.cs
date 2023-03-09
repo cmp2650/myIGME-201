5fd950d42bbe69eed15390c3f083043a95fbe33a
Chloe Pendergast <cmp2650@rit.edu>
2023-03-08 20:06:42 -0500

    PE14

---
+++ Pendergast_PE14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pendergast_PE14
+{
+    public interface myInterface
+    {
+        void interfaceMethod();
+    }
+
+    public class myClass : myInterface
+    {
+        public void interfaceMethod()
+        {
+            Console.WriteLine("output 1 = myClass");
+        }
+    }
+
+    public class yourClass : myInterface
+    {
+        public void interfaceMethod()
+        {
+            Console.WriteLine("output 2 = yourClass");
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            myClass my = new myClass();
+            yourClass your = new yourClass();
+            MyMethod(my);
+            MyMethod(your);
+        }
+
+        public static void MyMethod(object myObject)
+        {
+            myInterface myFace;
+            myFace = myObject as myInterface;
+            myFace.interfaceMethod();
+        }
+    }
+}
