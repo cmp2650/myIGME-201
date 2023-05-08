634acb2b67ee03767f96345998dae582483ceb12
Chloe Pendergast <cmp2650@rit.edu>
2023-05-07 23:36:54 -0400

    adjancency matrix & adjacency list

---
+++ Pendergast_FinalExam-3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pendergast_FinalExam_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0 = red, 1 = gray, 2 = dark blue, 3 = light blue, 4 = orange, 5 = yellow, 6 = purple, 7 = green
+            int[,] adjacencyMatrix = {
+                {0, 5, 1, 0, 0, 0, 0, 0}, // 0
+                {0, 0, 0, 0, 1, 0, 0, 0}, // 1
+                {0, 0, 0, 1, 0, 0, 0, 0}, // 2
+                {0, 0, 1, 0, 0, 0, 0, 0}, // 3
+                {0, 0, 0, 0, 0, 0, 1, 0}, // 4
+                {0, 0, 0, 0, 0, 0, 1, 6}, // 5
+                {0, 0, 0, 0, 0, 1, 0, 0}, // 6
+                {0, 0, 0, 0, 0, 1, 0, 0}, // 7
+            };
+
+            // converts array into list
+            List<(int, int)> edgeList = new List<(int, int)>();
+
+            for (int i = 0; i < adjacencyMatrix.GetLength(0); i++)
+            {
+                for (int j = i; j < adjacencyMatrix.GetLength(1); j++)
+                {
+                    if (adjacencyMatrix[i, j] != 0)
+                    {
+                        edgeList.Add((i + 1, j + 1));
+                    }
+                }
+            }
+        }
+    }
+}
